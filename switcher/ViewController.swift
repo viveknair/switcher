c5e8514da30796fe8e516d2cad29622ef9069d0a
Vivek Nair <vnair611@gmail.com>
2022-04-28 19:53:33 -0400

    Initial Commit

---
+++ switcher/ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  switcher
+//
+//  Created by Vivek Nair on 4/28/22.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+
+    // Do any additional setup after loading the view.
+  }
+
+  override var representedObject: Any? {
+    didSet {
+    // Update the view, if already loaded.
+    }
+  }
+
+
+}
+
